25ec5df1e2785c9d85191f5515e2a96b0d672003
Alexander Degtiarov <alexander.degtiarov96@gmail.com>
2021-05-23 19:16:32 +0300

    some changes

--- proj/directory/models.py
+++ proj/directory/models.py
@@ -16,25 +16,6 @@ class Valuta(models.Model):
         return self.name
 
 
-class Book(models.Model):
-    name = models.CharField(verbose_name="Название книги", max_length=100)
-    description = models.TextField()
-    price = models.IntegerField()
-    valuta = models.ForeignKey(Valuta, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.name
-
-
-class Series(models.Model):
-    name = models.CharField(verbose_name="Серия книг", max_length=100)
-    description = models.TextField(blank=True)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-
 class Genre(models.Model):
     name = models.CharField(verbose_name="Жанр", max_length=50)
     description = models.TextField()
@@ -46,6 +27,28 @@ class Genre(models.Model):
 class Publisher(models.Model):
     name = models.CharField(verbose_name="Издательство", max_length=100)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(verbose_name="Название книги", max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    valuta = models.ForeignKey(Valuta, on_delete=models.PROTECT)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING, null=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Series(models.Model):
+    name = models.CharField(verbose_name="Серия книг", max_length=100)
+    description = models.TextField(blank=True)
+    books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
